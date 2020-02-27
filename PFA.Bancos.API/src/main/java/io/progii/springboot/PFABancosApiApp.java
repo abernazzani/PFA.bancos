2c99c8c34149905708128398d9b447b460363499
Brian Desiderio <bdesiderio@gruposabra.com>
2020-02-27 09:48:50 -0300

    Se prepara el proyecto para la compilacion final

--- PFA.Bancos.API/src/main/java/io/progii/springboot/PFABancosApiApp.java
+++ PFA.Bancos.API/src/main/java/io/progii/springboot/PFABancosApiApp.java
@@ -1,13 +1,26 @@
 package io.progii.springboot;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PFABancosApiApp {
+public class PFABancosApiApp  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PFABancosApiApp.class, args);	;
+		SpringApplication app = new SpringApplication(PFABancosApiApp.class);
+		
+		app.setDefaultProperties(Collections
+		          .singletonMap("server.port", "80"));
+		        app.run(args);
 	}
+	
+	 @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PFABancosApiApp.class);
+     }
 
 }
